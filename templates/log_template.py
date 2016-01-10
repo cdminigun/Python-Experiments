9de7c6f42209baa9ee64e278f3dc29f8727e8caa
Christopher Doege <cdminigun@gmail.com>
2016-01-10 13:15:42 -0600

    Finished getopt logging.

--- templates/log_template.py
+++ templates/log_template.py
@@ -4,21 +4,19 @@ from time import gmtime, strftime
 def main():
     usage = "usage: %prog [options] arg"
     parser = OptionParser(usage)
-    parser.set_defaults(gmt=True, temp_name="fileIO-Example")
+    parser.set_defaults(gmt=True, temp_name="log-template")
     parser.add_option("-f", "--file", dest="temp_name",
-                      help="Name of your output file")
-    parser.add_option("-g", "--gmt",
-                      action="store_true", dest="gmt",
-                      help="Sets the time format for file creation to GMT")
+                      help="Name of output file: Default log-template")
     parser.add_option("-l", "--local",
                       action="store_false", dest="gmt",
-                      help="Sets the time format for file creation to local time")
+                      help="Sets the time format for file creation to your \
+                      local time: Default is GMT")
     (options, args) = parser.parse_args()
     if options.gmt:
         filename = strftime("%b-%d-%Y-%H:%M:%S-", gmtime())+ str(options.temp_name)
     else:
         filename = strftime("%b-%d-%Y-%H:%M:%S-") + str(options.temp_name)
-    print filename
-
+    file = open(filename, 'w')
+    
 if __name__ == "__main__":
     main()
