b589a7c0d2d3c39fb014b74ef7102d205719fdaa
Christopher Doege <cdminigun@gmail.com>
2016-04-20 21:49:55 -0500

    Added a counter for number of warnings.

--- bitly/bitlyWebsites.py
+++ bitly/bitlyWebsites.py
@@ -2,8 +2,9 @@ import pycurl
 import re
 from StringIO import StringIO
 
-letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 buffer = StringIO()
+i = 0
 c = pycurl.Curl()
 for char in letters:
     bitly = "bit.ly/" + char
@@ -13,8 +14,10 @@ for char in letters:
     body = buffer.getvalue()
 for line in body.split('\n'):
     if re.search('<body><a href="', line):
+        if re.search('https://bitly.com/a/warning', line):
+           i += 1
         print line
-
+print "The total number of warning sites is: ", i
 print "done"
 
 
