4d9ff07b8405a284d30f03c9f3e7eff95871b4be
Christopher Doege <cdminigun@gmail.com>
2016-04-21 02:38:40 -0500

    Added more functionality and permuted through more links. The run time is getting excessively large, as such I need to think of ways to improve my algo.

--- bitly/bitlyWebsites.py
+++ bitly/bitlyWebsites.py
@@ -5,19 +5,24 @@ from StringIO import StringIO
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 buffer = StringIO()
 i = 0
+total = 0
 c = pycurl.Curl()
 for char in letters:
-    bitly = "bit.ly/" + char
-    c.setopt(c.URL, bitly)
-    c.setopt(c.WRITEDATA, buffer)
-    c.perform()
-    body = buffer.getvalue()
+    for x in letters:
+        bitly = ""
+        bitly = "bit.ly/" + char + x
+        c.setopt(c.URL, bitly)
+        c.setopt(c.WRITEDATA, buffer)
+        c.perform()
+        body = buffer.getvalue()
 for line in body.split('\n'):
     if re.search('<body><a href="', line):
+        total += 1
         if re.search('https://bitly.com/a/warning', line):
            i += 1
         print line
 print "The total number of warning sites is: ", i
+print "The total number of sites is: ", total
 print "done"
 
 
