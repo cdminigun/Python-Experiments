4eea78d62fcf5422edaa08221259f692b9f4cb85
cdminigun <doege.chris@yahoo.com>
2017-03-04 22:41:38 -0600

    Added sample with array of buttons.

---
+++ qt_examples/sample2.py
@@ -0,0 +1,21 @@
+import sys
+from PyQt4 import QtGUi
+class GridLayout(QtGui.QWidget):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.setWindowTitle('grid layout')
+        names = [ '7', '8', '9', '4', '5', '6']
+        grid = QtGui.QGridLayout()
+        j = 0
+        pos = [(0, 0), (0, 1), (0, 2), (1, 0), (1,1),(1,2)]
+        for i in names:
+            button = QtGui.QPushButton(i)
+            grid.addWidget(button, pos[j][0], pos[j][1])
+            j = j + 1
+            self.setLayout(grid)
+app = QtGui.QApplication(sys.argv)
+qb = GridLayout()
+qb.show()
+sys.exit(app.exec_())
+
+
