994adc97a57c4833a7b9f6366900792104850f38
cdminigun <doege.chris@yahoo.com>
2017-03-06 19:44:45 -0600

    Added colatz recursive example in python.

---
+++ colatz.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+# ---------------------------
+# projects/collatz/Collatz.py
+# Copyright (C) 2015
+# Glenn P. Downing
+# ---------------------------
+
+# ------------
+# collatz_read
+# ------------
+
+def collatz_read (s) :
+    """
+    read two ints
+    s a string
+    return a list of two ints, representing the beginning and end of a range, [i, j]
+    """
+    a = s.split()
+    return [int(a[0]), int(a[1])]
+
+# ------------
+# collatz_eval
+# ------------
+
+def collatz_eval (i, j) :
+    """
+    i the beginning of the range, inclusive
+    j the end       of the range, inclusive
+    return the max cycle length of the range [i, j]
+    """
+    # <your code>
+    return 1
+
+# -------------
+# collatz_print
+# -------------
+
+def collatz_print (w, i, j, v) :
+    """
+    print three ints
+    w a writer
+    i the beginning of the range, inclusive
+    j the end       of the range, inclusive
+    v the max cycle length
+    """
+    w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
+
+# -------------
+# collatz_solve
+# -------------
+
+def collatz_solve (r, w) :
+    """
+    r a reader
+    w a writer
+    """
+    for s in r :
+        i, j = collatz_read(s)
+        v    = collatz_eval(i, j)
+        collatz_print(w, i, j, v)
+ 
+ 
+ 
