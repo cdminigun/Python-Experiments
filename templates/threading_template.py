4b06e0636b16506d6bfdcb3a87048bd68e5bc0a5
cdminigun <doege.chris@yahoo.com>
2017-09-21 22:10:59 -0400

    Added complete threading template that handles edge cases.

--- templates/threading_template.py
+++ templates/threading_template.py
@@ -1,4 +1,6 @@
 import threading
+import os
+import sys
 
 # Threading events allow for a signal to be sent across several threads
 thread_wrangler = threading.Event()
@@ -9,17 +11,39 @@ def thread_func():
 
 
 def main():
-    # Start 10 threads
+    #Create a list of 10 threads, allows us to join on all threads
+    threads = []
     for i in xrange(10):
-        thread = threading.Thread(target=thread_func, args=(), name="thread%s" % i)
+        threads.append(threading.Thread(target=thread_func, args=(), name="thread%s" % i))
+    # Start 10 threads
+    for thread in threads:
         thread.start()
     # Join threads with error checking and to allow for non-blocking
     # interrupts across multiple threads in python.
     try:
-        while thread.is_alive():
-            thread.join(timeout=1)
+        for thread in threads:
+            while thread.is_alive():
+                # Sends keyboard interrupt if threading event is set.
+                # This ensures that anything proceeding in the function
+                # is safe to execute.
+                #if thread_wrangler.is_set():
+                #    os.kill(os.getpid(), signal.SIGINT)
+                thread.join(timeout=1)
     # FIXME: Add more interrupts/exceptions if needed
     except KeyboardInterrupt:
         thread_wrangler.set()
+        try:
+            # Wait for graceful shutdown of threads.
+            for thread in threads:
+                while thread.is_alive():
+                    thread.join(timeout=1.0)
+                #Cleanup has finished
+        #Case that we want to exit immediately and not cleanup
+        except (KeyboardInterrupt, Exception) as e:
+            #Uses protected class to force exit of all threads
+            os._exit(-1)
+        sys.exit(-1)
+    print "Everything has finished!"
+
 if __name__ == "__main__":
     main()
